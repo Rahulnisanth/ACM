a963b6f420d7c7ffbe9c9a7efb629e6a522086dd
Rahulnisanth <rahulnisanth@abovecloud9.ai>
2025-01-09 21:03:03 +0530

    added: automated readme file creator

---
+++ src/utils/startProjectTracking.js
@@ -0,0 +1,25 @@
+const vscode = require("vscode");
+const { mkdirSync, existsSync } = require("fs");
+const { join } = require("path");
+const createReadmeLogs = require("./createReadmeLogs");
+
+function startProjectTracking(currentProjectPath) {
+  const projectHistoryPath = join(currentProjectPath, "project-history");
+  try {
+    if (!existsSync(projectHistoryPath)) {
+      mkdirSync(projectHistoryPath);
+      vscode.window.showInformationMessage(
+        `'project-history' folder created successfully at ${projectHistoryPath}`
+      );
+    }
+    setInterval(() => {
+      createReadmeLogs(currentProjectPath, projectHistoryPath);
+    }, 1 * 60 * 1000);
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      `Failed to create 'project-history' folder: ${error.message}`
+    );
+  }
+}
+
+module.exports = startProjectTracking;
